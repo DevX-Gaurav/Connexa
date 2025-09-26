9c68b9826955d633e6454d4c74a77bc8364ad21b
Gaurav_Jha <jhagaurav82003@gmail.com>
2025-09-26 23:31:51 +0530

    backend for now is completed

--- server/index.js
+++ server/index.js
@@ -4,21 +4,45 @@ import cookieParser from "cookie-parser";
 import { ConnectDb } from "./Config/db.js";
 import bodyParser from "body-parser";
 import userAuthRoute from "./routes/user.auth.route.js";
+import chatRoute from "./routes/chat.route.js";
+import statusRoute from "./routes/status.route.js";
+import cors from "cors";
+import initializeSocket from "./services/socket.service.js";
+import http from "http";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const corsOption = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
 
 /* middlewares */
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/* create server */
+const server = http.createServer(app);
+const io = initializeSocket(server);
+
+/* apply socket middleware before all routes */
+app.use((req, res, next) => {
+  req.io = io;
+  req.socketUserMap = io.socketUserMap;
+  next();
+});
+
 /* routes */
 app.use("/api/auth", userAuthRoute);
+app.use("/api/chat", chatRoute);
+app.use("/api/status", statusRoute);
 
 ConnectDb().then(() => {
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}.`);
   });
 });
